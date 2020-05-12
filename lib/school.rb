b1813f8d10251ec60f121abca5c50fd64e5f7f8c
Chris Hosler <cmstie@gmail.com>
2020-05-12 22:08:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(grade, name)
+    @roster[:grade] = grade
+    @roster[:grade] << name
+    
+  end
+  
+end
